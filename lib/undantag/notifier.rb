3acc3bf2a426bd2dac4e1f2abb263a40fcf494da
Magnus Hörberg <magnus.hoerberg@gmail.com>
2012-10-20 17:14:26 +0200

    changed so that notify takes both the request and the environment

--- lib/undantag/notifier.rb
+++ lib/undantag/notifier.rb
@@ -1,14 +1,16 @@
 module Undantag
   class Notifier
-    URL = "http://undantag.duh.se/exception"
-    def self.notify exception
+    URL = "https://undantag.herokuapp.com/exception"
+    def self.notify request, exception
       unless Undantag::Configuration.api_key
         raise Undantag::ConfigurationError::NoApiKey
       end
       uri = URI(Notifier::URL)
       key = Undantag::Configuration.api_key
-      post_params = Undantag::Configuration.to_hash.merge(title: 'test',
-                                                          body: exception)
+      post_params = Undantag::Configuration.to_hash.merge(env: ENV,
+                                                          request: request,
+                                                          exception: exception)
+
       resp = Net::HTTP.post_form(uri, post_params)
       case resp.code
       when "401"
--- spec/undantag/notifier_spec.rb
+++ spec/undantag/notifier_spec.rb
@@ -16,19 +16,19 @@ describe Undantag::Notifier do
       Undantag.configure(api_key: ENV['UNDANTAG_API_KEY'],
                          github_user: ENV['GITHUB_USER'],
                          github_repo: ENV['GITHUB_REPO'])
-      Undantag::Notifier.notify(Exception.to_s)
+      Undantag::Notifier.notify(nil, Exception.to_s)
     end
   end
   it 'throws Undantag::NotAuthorized when a 401 is returned' do
     VCR.use_cassette("undantag-server-not-authorized") do
       Undantag.configure api_key: ENV['UNDANTAG_API_KEY']
-      expect { Undantag::Notifier.notify(Exception.to_s) }.to raise_error(Undantag::NotAuthorized)
+      expect { Undantag::Notifier.notify(nil, Exception.to_s) }.to raise_error(Undantag::NotAuthorized)
     end
   end
   it 'throws "Undantag::ConfigurationError::NoApiKey" if send is called' do
     #temp hack
     Undantag.configure api_key: nil
-    expect { Undantag::Notifier.notify(Exception.to_s) }.to raise_error(Undantag::ConfigurationError::NoApiKey)
+    expect { Undantag::Notifier.notify(nil, Exception.to_s) }.to raise_error(Undantag::ConfigurationError::NoApiKey)
   end
 end
 
